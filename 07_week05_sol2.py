f4a39302a832dbb6ca97395eff210bbae7b52fba
Sang-Yeong <thf0415@naver.com>
2020-08-02 18:43:10 +0900

    upload 07_week5

--- 07_week05_sol2.py
+++ 07_week05_sol2.py
@@ -1,18 +1,17 @@
+# 7월 5주차 문제풀이 - 2. 크레인 인형뽑기 게
+
 def solution(board, moves):
     answer = 0
     basket = []
     n = len(board[0])
             
     for i in moves:
-        print(i,'-moves')
         for j in range(n):
-            print(j,'-board')
             if board[j][i-1] == 0:
                 continue
             else:
                 doll = board[j][i-1]
                 board[j][i-1] = 0
-                print('board=',board)
                 break
             
         if basket == []:
@@ -23,9 +22,4 @@ def solution(board, moves):
             answer += 1
         else:
             basket.append(doll)
-        print('basket = ', basket,'\n')
     return answer
-
-board = [[0,0,0,0,0],[0,0,1,0,3],[0,2,5,0,1],[4,2,4,4,2],[3,5,1,3,1]]
-moves = [1,5,3,5,1,2,1,4]
-print('result = ',solution(board, moves))
