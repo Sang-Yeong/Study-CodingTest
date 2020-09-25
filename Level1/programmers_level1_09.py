e04b805901e8760961ad69afc1433086a63f813f
Sang-Yeong <thf0415@naver.com>
2020-09-25 22:41:24 +0900

    가운데 글자 가져오기

---
+++ Level1/programmers_level1_09.py
@@ -0,0 +1,10 @@
+# 가운데 글자 가져오기
+
+def solution(s):
+    answer = ''
+    
+    if len(s) % 2 :
+        answer += s[len(s) // 2 ]
+    else:
+        answer += s[len(s)//2 - 1 : len(s)//2 + 1]
+    return answer
