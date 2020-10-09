5fe7de2e1604489e00b71559c801259d33a5b207
Sang-Yeong <thf0415@naver.com>
2020-10-09 21:10:28 +0900

    두 정수 사이의 합

--- Level1/programmers_level1_14.py
+++ Level1/programmers_level1_14.py
@@ -1,10 +1,9 @@
-# 서울에서 김서방 찾기
+# 두 정수 사이의 합
 
-def solution(seoul):
-    answer = ''
+def solution(a, b):
     
-    for i in range(len(seoul)):
-        if 'Kim' == seoul[i]:
-            answer += '김서방은 {}에 있다'.format(i)
-    
-    return answer
+    if a > b:
+        a,b = b,a
+        
+    lst = [n for n in range(a, b+1)]
+    return sum(lst)
